024f29c0b866ff202d2f546d0379671850a0978f
Beatriz Fröhlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-04-29 08:28:35 -0300

    ADD: Iniciei as validações no javascript

--- script/script.js
+++ script/script.js
@@ -1,20 +1,44 @@
-function logar(){
-    var login = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
-    var mensagem = document.getElementById("mensagem");
+let form = document.getElementById('formulario');
 
-    if(usuario === ""){
-        mensagem.innerText = "Preencha este campo";
-        return;
-    }
-    if(usuario === ""){
-        mensagem.innerText = "Preencha este campo"
-        return;
-    }
-    if(usuario === "admin" && senha === "admin"){
-        location.href = "index/index.html";
-    }else{
-        mensagem.innerText = "Usuario ou senha invalidos"
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    let usuario = document.getElementById('usuario').Value;
+    let senha = document.getElementById('senha').value;
+
+    document.getElementById('erroUsuario').textContent = '';
+    document.getElementById('erroSenha').textContent = '';
+
+    if (usuario.lenght === admin) {
+        document.getElementById('erroUsuario').textContent = 'Nome de usuario incorreto';
+
+        valido = false;
+
     }
 
-}
+    if (!email.includes('@')) {
+        document.getElementById('erroEmail').textContent = 'Email Inválido, deve conter @.';
+        valido = false;
+    }
+
+    if (senha.lenght < 6) {
+        document.getElementById('erroSenha').textContent = 'Senha Inválida, deve conter no mínimo 6 caracteres';
+        valido = false;
+    }
+
+    if (valido) {
+        let resultado = document.getElementById('resultado');
+
+        resultado.innerHTML = `
+
+Dados enviados: <br>
+Nome: ${nome} <br>
+Email: ${email} <br>
+Senha: ${senha} <br>
+
+`;
+        form.reset();
+    }
+
+})
