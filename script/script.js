ea6aeee30ebef8cf4aa7bb0fe651f8657f4a9853
Beatriz Fröhlich <beatriz_frohlich@estudante.sesisenai.org.br>
2026-04-29 09:10:41 -0300

    ADD: Atualizei a pagina Home

--- script/script.js
+++ script/script.js
@@ -4,26 +4,26 @@ form.addEventListener('submit', function (e) {
 
     e.preventDefault();
 
-    let usuario = document.getElementById('usuario').Value;
+    let usuario = document.getElementById('usuario').value;
     let senha = document.getElementById('senha').value;
+
+    let valido = true;
 
     document.getElementById('erroUsuario').textContent = '';
     document.getElementById('erroSenha').textContent = '';
 
-    if (usuario.lenght === admin) {
-        document.getElementById('erroUsuario').textContent = 'Nome de usuario incorreto';
-
-        valido = false;
-
+    if (usuario !== 'admin') {
+    let campo = document.getElementById('usuario');
+    campo.classList.add('is-invalid');
+    document.getElementById('erroUsuario').textContent = 'Usuário incorreto';
+    valido = false;
+    } else {
+    campo.classList.remove('is-invalid');
+    campo.classList.add('is-valid');
     }
 
-    if (!email.includes('@')) {
-        document.getElementById('erroEmail').textContent = 'Email Inválido, deve conter @.';
-        valido = false;
-    }
-
-    if (senha.lenght < 6) {
-        document.getElementById('erroSenha').textContent = 'Senha Inválida, deve conter no mínimo 6 caracteres';
+    if (senha.length === 1234) {
+        document.getElementById('erroSenha').textContent = 'Senha deve ter no mínimo 6 caracteres';
         valido = false;
     }
 
@@ -31,14 +31,13 @@ form.addEventListener('submit', function (e) {
         let resultado = document.getElementById('resultado');
 
         resultado.innerHTML = `
+            <div class="alert alert-success mt-3">
+                Dados enviados:<br>
+                Usuário: ${usuario}<br>
+                Senha: ${senha}
+            </div>
+        `;
 
-Dados enviados: <br>
-Nome: ${nome} <br>
-Email: ${email} <br>
-Senha: ${senha} <br>
-
-`;
         form.reset();
     }
-
-})
+});
